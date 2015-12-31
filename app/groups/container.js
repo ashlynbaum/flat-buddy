920c54d99c0623a216537e88c577bc8250b975d8
Ashlyn Baum <a@ashlynbaum.com>
2015-12-31 15:58:52 +1300

    [WIP] group index view.

--- app/groups/container.js
+++ app/groups/container.js
@@ -1,13 +1,25 @@
 const React = require('react')
 const { connect } = require('react-redux')
 
+// let groupsData = [{name: "group1"}, {name: "group2"}, {name: "group3"}]
 class GroupsContainer extends React.Component {
   render () {
     return <div>
       group list!
+      // groupData.map((group) => return <GroupItem props={group}></GroupItem>)
     </div>
   }
 }
+
+// class GroupItem extends React.Component {
+// 	render() {
+// 		return	<div>
+//     		<ul>
+//     			<li>data.props.name</li>
+//     		</ul>
+//     	</div>
+//     }
+// }
 
 module.exports = connect(
   (state) => ({})
--- app/groups/routes.js
+++ app/groups/routes.js
@@ -4,5 +4,5 @@ const { Route } = require('react-router')
 const GroupsContainer = require('./container')
 
 module.exports = function createRoutes (store) {
-  return <Route path="/" component={GroupsContainer} />
+  return <Route path="/groups" component={GroupsContainer} />
 }
