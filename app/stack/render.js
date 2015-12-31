71170e39e5615e3aac587b883c62ffa23a18694b
Ashlyn Baum <a@ashlynbaum.com>
2015-12-31 14:27:34 +1300

    Update style of navbar.

--- app/stack/render.js
+++ app/stack/render.js
@@ -56,6 +56,7 @@ function renderFullPage (innerHtml, initialData) {
         <meta charset="utf-8" />
         <title>Happy Flat :)</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href='https://fonts.googleapis.com/css?family=Amatic+SC:400,700' rel='stylesheet' type='text/css'>
       </head>
       <body>
         <main>${ innerHtml }</main>
