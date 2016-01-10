a9594e6bcac2d2ce1a99acc3850d8555da81766c
Ashlyn Baum <a@ashlynbaum.com>
2016-01-10 20:21:25 +1300

    Change combined reducer to convert to camelcase and use lowdash

--- app/reducer.js
+++ app/reducer.js
@@ -2,15 +2,26 @@ const bulk = require('bulk-require')
 const { combineReducers } = require('redux')
 const { routeReducer } = require('redux-simple-router')
 const { map } = require('ramda')
+const { mapKeys, mapValues, assign, camelCase } = require('lodash')
 
-module.exports = combineReducers({
-  ...map(
-    (module) => module.reducer,
-    bulk(__dirname, '*/reducer.js')
+module.exports = combine(assign(
+  mapValues(
+    bulk(__dirname, '*/reducer.js'),
+    (module) => module.reducer
   ),
-  ...map(
-    (module) => combineReducers(module.reducers),
-    bulk(__dirname, '*/reducers/*.js')
-  ),
-  routing: routeReducer
-})
+  //mapValues(
+  //  bulk(__dirname, '*/reducers/*.js'),
+  //  (module) => combineReducers(module.reducers)
+  //),
+  {
+    routing: routeReducer
+  }
+))
+
+function combine (reducers) {
+  return combineReducers(
+    mapKeys(reducers, function (reducer, name) {
+      return camelCase(name)
+    })
+  )
+}
