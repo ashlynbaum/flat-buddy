2349dcaf8bad6fcd3c05569ff46ba8d63db45caa
Ashlyn Baum <a@ashlynbaum.com>
2016-01-10 20:22:00 +1300

    Reroute location of shopping cart to shopping-list directory.

--- app/groups/routes.js
+++ app/groups/routes.js
@@ -4,7 +4,7 @@ const { IndexRoute, Route } = require('react-router')
 const IndexContainer = require('./containers/index')
 const ShowContainer = require('./containers/show')
 const GroupMembers = require('./components/group-members')
-const ShoppingList = require('./components/shopping-list')
+const ShoppingList = require('app/shopping-list/components/shopping-list')
 
 module.exports = function createRoutes (store) {
   return <Route path="groups">
